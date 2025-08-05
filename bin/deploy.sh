415043732f4b8899f6f81df2a4876dba2c8c43cb
Josh Long <54473+joshlong@users.noreply.github.com>
2025-08-04 17:59:19 -0700

    up

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -17,11 +17,6 @@ write_secrets(){
   export SECRETS_NAME=${SERVICE}-secrets.env
   export SECRETS_FN=`pwd`/${SECRETS_NAME}
   
-  echo "going to create ${SECRETS_NAME} secrets in the $NS namespace."
-
-  # get each  of the keys passed to the plugin and use them to resolve 
-  # the value and write them to a k8s secrets object
-  
   SECRET_VARS=""
   
   IFS=',' read -ra KEY_ARRAY <<< "$ENV_KEYS"
@@ -32,9 +27,6 @@ write_secrets(){
     echo "${key}=${value}" >> $SECRETS_FN
   done
 
-  
-  cat $SECRETS_FN
-
   kubectl delete secrets -n $NS $SECRETS_NAME || echo "no secrets to delete."
   kubectl create secret generic $SECRETS_NAME -n $NS --from-env-file $SECRETS_FN
 
@@ -44,58 +36,45 @@ get_image(){
   kubectl get "$1" -o json  | jq -r  ".spec.template.spec.containers[0].image" || echo "no old version to compare against"
 }
 
-echo "the namespace is $NS"
-echo "the action path is ${ROOT_DIR}"
 
 gcloud config set project $GKE_PROJECT
-gcloud --quiet auth configure-docker
 gcloud auth configure-docker us-docker.pkg.dev --quiet
 kubectl get ns/${NS} || kubectl create namespace ${NS}
 kubectl config set-context --current --namespace=${NS}
 kubectl get pods
 
 write_secrets
-echo "root dir is $ROOT_DIR "
-ls -la $ROOT_DIR
 cd $ROOT_DIR 
 
 APP_YML=${ROOT_DIR}/bin/k8s/carvel/app-app-data.yml
-ls -la `dirname $APP_YML`
-ls -la $ROOT_DIR/bin/manifest_gen/main.py
 $ROOT_DIR/bin/manifest_gen/main.py > ${APP_YML}
-cat ${APP_YML}
-ls -la `dirname $APP_YML`
 
 
-export NAMESPACE_NAME=$NS
-
-# # MAIN APPS
-# # and there are a bunch of apps we needs to deploy and they all share a similar setup
-for f in app  ; do
-  
-  echo "------------------"
-
-  IP=${NAMESPACE_NAME}-${f}-ip
-  echo "creating IP called ${IP} "
-  create_ip $IP
-  echo "created IP called ${IP} "
+for f in app ; do
+  IP=${NS}-${f}-ip
   Y=${ROOT_DIR}/bin/k8s/carvel/app-${f}-data.yml
   D=deployments/${f}-deployment
-  OLD_IMAGE=`get_image $D `
   OUT_YML=out.yml
   ytt -f $Y -f "$ROOT_DIR"/bin/k8s/carvel/data-schema.yml -f "$ROOT_DIR"/bin/k8s/carvel/deployment.yml |  kbld -f -  > ${OUT_YML}
+
+  cat ${OUT_YML} | kubectl apply  -n ${NS} -f -
+
+  echo "--------------------------"
+  echo "Final Kubernetes YAML:"
+  echo "--------------------------"
   cat ${OUT_YML}
-  cat ${OUT_YML} | kubectl apply  -n $NAMESPACE_NAME -f -
-  NEW_IMAGE=`get_image $D`
-  echo "comparing container images for the first container!"
-  echo $OLD_IMAGE
-  echo $NEW_IMAGE
-  if [ "$OLD_IMAGE" = "$NEW_IMAGE" ]; then
-    echo "no need to restart $D"
-  else
-   echo "restarting $D"
-   kubectl rollout restart $D
-  fi
+  echo "--------------------------"
+
+  # NEW_IMAGE=`get_image $D`
+  # echo "comparing container images for the first container!"
+  # echo $OLD_IMAGE
+  # echo $NEW_IMAGE
+  # if [ "$OLD_IMAGE" = "$NEW_IMAGE" ]; then
+    # echo "no need to restart $D"
+  # else
+   # echo "restarting $D"
+   # kubectl rollout restart $D
+  # fi
 
 done
 
